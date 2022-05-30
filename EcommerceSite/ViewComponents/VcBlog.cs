2396d7f3599de07625e8aed98d843bb193952dd7
EminElekberov <eminelekberov09@gmail.com>
2022-05-30 23:11:23 +0400

    update

---
+++ EcommerceSite/ViewComponents/VcBlog.cs
@@ -0,0 +1,31 @@
+﻿using EcommerceSite.Models;
+using EcommerceSite.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcommerceSite.ViewComponents
+{
+    public class VcBlog : ViewComponent
+    {
+        private readonly SyteDbContext dbContext;
+        public VcBlog(SyteDbContext _dbContext)
+        {
+            dbContext = _dbContext;
+        }
+        public IViewComponentResult Invoke()
+        {
+            HomeVm home = new HomeVm
+            {
+                Blogs = dbContext.Blogs.Include(x => x.category).ToList(),
+                Categories = dbContext.Categories.Include(x => x.Products).ToList()
+            };
+
+            return View(home);
+        }
+
+    }
+}
---
+++ EcommerceSite/ViewComponents/VcCategory.cs
@@ -0,0 +1,24 @@
+﻿using EcommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcommerceSite.ViewComponents
+{
+    public class VcCategory: ViewComponent
+    {
+        private readonly SyteDbContext dbContext;
+        public VcCategory(SyteDbContext _dbContext)
+        {
+            dbContext = _dbContext;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var model = dbContext.Categories.ToList();
+            return View(model);
+        }
+    }
+}
