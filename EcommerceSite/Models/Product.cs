043c543fb8e8c22aa0291ba93065084038c450c1
EminElekberov <eminelekberov09@gmail.com>
2022-08-01 23:37:49 +0400

    first comit

--- EcommerceSite/Models/Product.cs
+++ EcommerceSite/Models/Product.cs
@@ -4,10 +4,8 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
-
 namespace EcommerceSite.Models
 {
-
     public class Product
     {
         public int Id { get; set; }
