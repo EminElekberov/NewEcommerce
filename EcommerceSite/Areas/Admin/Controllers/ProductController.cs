9d25690b16c65cc5f1ef53fd2d8809e1f2338aa7
EminElekberov <eminelekberov09@gmail.com>
2022-08-05 22:30:06 +0400

    first comit

--- EcommerceSite/Areas/Admin/Controllers/ProductController.cs
+++ EcommerceSite/Areas/Admin/Controllers/ProductController.cs
@@ -14,6 +14,7 @@ using System.Threading.Tasks;
 namespace EcommerceSite.Areas.Admin.Controllers
 {
     [Area("Admin")]
+
     public class ProductController : Controller
     {
         private readonly SyteDbContext _dbcontext;
